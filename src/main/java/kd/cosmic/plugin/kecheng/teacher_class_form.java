a03e3604cfb06929944df0d0d7dbc400e5587b24
superwateratom <2622804072@qq.com>
2023-08-04 18:11:14 +0800

    test

--- src/main/java/kd/cosmic/plugin/kecheng/teacher_class_form.java
+++ src/main/java/kd/cosmic/plugin/kecheng/teacher_class_form.java
@@ -1,31 +1,46 @@
 package kd.cosmic.plugin.kecheng;
 
 
+import com.google.gson.Gson;
+import kd.bos.context.RequestContext;
 import kd.bos.db.DB;
 import kd.bos.db.DBRoute;
+import kd.bos.ext.form.control.CustomControl;
 import kd.bos.form.plugin.AbstractFormPlugin;
 import kd.sdk.plugin.Plugin;
-import java.util.ArrayList;
-import java.util.EventObject;
-import java.util.List;
+
+
+import java.math.BigInteger;
+import java.util.*;
 
 /**
  * 动态表单
  */
 public class teacher_class_form extends AbstractFormPlugin implements Plugin {
+    public static final String filed_key = "";
     @Override
     public void afterCreateNewData(EventObject e) {
         
-        String sql = "SELECT fk_yt77_option_number FROM tk_yt77_choose_list";
-        DB.queryDataSet("test",DBRoute.basedata,sql);
-        //Object query = DB.query(DBRoute.basedata, sql, rs -> {
-            List<String> idList = DB.query(DBRoute.basedata, sql,rs -> {
-                List<String> ret = new ArrayList<>();
-                while (rs.next()) {
-                    ret.add(rs.getString(1));
-                }
-                return ret;
-            });
-        }
+
+        String query_sql = "SELECT fk_yt77_week,fk_yt77_name,fk_yt77_lessons FROM tk_yt77_choose_list WHERE fcreatorid=?";
+
+        List<Map<String, Object>> query = DB.query(DBRoute.basedata, query_sql,new Object[]{new BigInteger(RequestContext.get().getUserId())}, rs -> {
+            List<Map<String, Object>> mapList = new ArrayList<>();
+            while (rs.next()) {
+                Map<String, Object> objectMap = new HashMap<>();
+                objectMap.put("xingqi", rs.getInt(1));
+                objectMap.put("classname",rs.getString(2));
+                objectMap.put("data",rs.getInt(3));
+                mapList.add(objectMap);
+            }
+            return mapList;
+        });
+        CustomControl control = this.getView().getControl(filed_key);
+        control.setData(query);
+        Gson gson = new Gson();
+        String s = gson.toJson(query);
+        System.out.println(s);
+    }
+
 
 }
